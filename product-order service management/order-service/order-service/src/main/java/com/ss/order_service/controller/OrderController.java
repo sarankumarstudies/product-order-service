d928d4cea56c03c5793a0de4148d2c6d2bd642bd
sarankumarstudies <sarankumarstudies@gmail.com>
2025-11-02 08:44:25 +0530

    buiild (Eureka Server) : Cofigured the erureka server.

--- product-order service management/order-service/order-service/src/main/java/com/ss/order_service/controller/OrderController.java
+++ product-order service management/order-service/order-service/src/main/java/com/ss/order_service/controller/OrderController.java
@@ -22,29 +22,29 @@ public class OrderController {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
-    @PostMapping("/placeOrder")
-    public Mono<ResponseEntity<OrderResponseDto>> placeOrder(@RequestBody Order order) {
+        @PostMapping("/placeOrder")
+        public Mono<ResponseEntity<OrderResponseDto>> placeOrder(@RequestBody Order order) {
 
 
-        return webClientBuilder.build().get().uri("http://localhost:8081/products/" + order.getProductId()).retrieve().bodyToMono(ProductResponseDto.class).map(productResponseDto -> {
-            OrderResponseDto responseDto = new OrderResponseDto();
+            return webClientBuilder.build().get().uri("http://PRODUCT-SERVICE/products" + order.getProductId()).retrieve().bodyToMono(ProductResponseDto.class).map(productResponseDto -> {
+                OrderResponseDto responseDto = new OrderResponseDto();
 
-            responseDto.setProductId(order.getProductId());
-            responseDto.setQuantity(order.getQuantity());
-            responseDto.setProductName(productResponseDto.getName());
-            responseDto.setProductPrice(productResponseDto.getPrice());
+                responseDto.setProductId(order.getProductId());
+                responseDto.setQuantity(order.getQuantity());
+                responseDto.setProductName(productResponseDto.getName());
+                responseDto.setProductPrice(productResponseDto.getPrice());
 
-            double totalPrice = order.getQuantity() * productResponseDto.getPrice();
-            responseDto.setTotalPrice(totalPrice);
-            order.setPrice(totalPrice);
-            order.setName(productResponseDto.getName());
-            orderRepository.save(order);
+                double totalPrice = order.getQuantity() * productResponseDto.getPrice();
+                responseDto.setTotalPrice(totalPrice);
+                order.setPrice(totalPrice);
+                order.setName(productResponseDto.getName());
+                orderRepository.save(order);
 
-            responseDto.setOrderId(order.getId());
-            return ResponseEntity.ok(responseDto);
-        });
+                responseDto.setOrderId(order.getId());
+                return ResponseEntity.ok(responseDto);
+            });
 
-    }
+        }
 
     @GetMapping
     public List<Order> getAllOrders (){
